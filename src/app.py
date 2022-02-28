9d17e65e5ca8d0dcb3431acc4166fb8ce21d635b
Vihaan Tarale <taralevihaan@gmail.com>
2022-02-28 21:50:45 +0530

    still writing

---
+++ src/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request, render_template, redirect, url_for, send_from_directory
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.secret_key = "ghdgjdfhgdfj"
+server = SocketIO(app)
+
+@app.route("/")
+def index():
+    render_template("index.html")
+
+@app.route("/sendfile/<path:path>")
+def sendfile(path):
+    return send_from_directory('static', path)
