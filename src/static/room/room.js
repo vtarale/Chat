ea760ba7b97709c9e5bddcf5383d9adf2d64f460
Vihaan Tarale <taralevihaan@gmail.com>
2022-03-03 12:21:33 +0530

    added some security fixes, still writing room

--- src/static/room/room.js
+++ src/static/room/room.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const input = document.querySelector('input');
     const list = document.querySelector('ul');
     const leave = document.querySelector('#leave');
-    var socket = io(location.protocol + '//' + document.domain + ':' + location.port);
 
     param = new URLSearchParams(window.location.search);
     room = param.get('room');
@@ -16,6 +15,8 @@ document.addEventListener('DOMContentLoaded', function() {
     if (not_json != 1){
         window.location.replace('/room_password?room=' + room + '&name=' + n);
     }
+
+    var socket = io(location.protocol + '//' + document.domain + ':' + location.port);
 
     socket.on('connect', function() {
         socket.emit('join', {'roomid': room, 'name': n});
